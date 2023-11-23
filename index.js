9cac60667ef8300cd97111c0f20cba2c51170121
Jesse Josiah <jessezephyr@gmail.com>
2023-11-24 00:10:09 +0100

    test

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 5002
+const port = 3000
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const jobRouter = require('./routes/jobs')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log('Connected to DB'))
-.catch(() => console.log('Error connecting to DB'));
+    .then(()=> console.log('Connect to JobHub Db'))
+    .catch((err)=> console.log(err));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/api/jobs', jobRouter)
 
-app.get('/', (req, res) => res.send('Testing World!'))
-app.listen(process.env.PORT || port, () => console.log(`Jobhunt listening on port ${process.env.PORT}!`))
+
+
+app.listen(process.env.PORT || port, () => 
+            console.log(`The Hub is listening on port ${process.env.PORT }!`))
