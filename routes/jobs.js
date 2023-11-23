bdfa906f84c9d874d2edf24d61bfac536055c653
Jesse Josiah <jessezephyr@gmail.com>
2023-11-23 23:57:39 +0100

    update

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
-const jobController = require('../controllers/jobControllers')
+const jobController =  require('../controllers/jobControllers');
 
-router.post('/', jobController.createJob)
-router.get('/', jobController.getAllJobs)
-router.get('/search/:key', jobController.searchJobs)
+router.post('/', jobController.createJob);
 
+router.get('/', jobController.getAllJobs);
 
-router.get('/:id', jobController.getJob)
-router.put('/:id', jobController.updateJob)
-router.delete('/:id', jobController.deleteJob)
+router.get('/search/:key', jobController.searchJobs);
 
+router.get('/:id', jobController.getJob);
+
+router.put('/:id', jobController.updateJob);
+
+router.delete('/:id', jobController.createJob);
 
 module.exports = router;
