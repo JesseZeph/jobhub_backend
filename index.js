20683dce05d327d417fcec56f6ef45c3d06c2391
Jesse Josiah <jessezephyr@gmail.com>
2023-11-24 02:28:46 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3020
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const jobRouter = require('./routes/job')
