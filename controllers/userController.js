51b20a72dc08623b43bc715fe272573063f6a3ec
JesseZeph <jessezephyr@gmail.com>
2023-12-24 10:15:06 +0100

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -78,7 +78,7 @@ module.exports = {
 
   addAgent: async (req, res) => {
     const newAgent = new Agent({
-      id: req.user.id,
+      userId: req.user.id,
       uid: req.body.uid,
       working_hrs: req.body.working_hrs,
       hq_address: req.body.hq_address,
